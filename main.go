dc45f34454b3dace808dcc8cd4498c58c8d4a82e
1730314864@qq.com <king1996>
2019-05-19 14:47:42 +0800

    update

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
-	"./modules/scanner"
-	// "fmt"
-	// "os"
+	"modules/scanner"
+
+	"fmt"
+	"time"
 )
 
 var (
-	website = ""
+	website = "http://www.lianhetech.com/"
 )
 
 func main() {
+	var start = time.Now().Unix()
 	scanner.Scan(website)
+	var end = time.Now().Unix()
+	fmt.Println("Time: ", end-start, "s")
 }
